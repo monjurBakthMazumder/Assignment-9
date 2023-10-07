b7e6c4894c04b3d74ea5a04597b56103fefaa94a
md Monjur Bakth Mazumder <md.monjurmbm2001@gmail.com>
2023-10-08 03:52:53 +0600

    update errorpage

--- src/Pages/ErrorPages/ErrorPages.jsx
+++ src/Pages/ErrorPages/ErrorPages.jsx
@@ -1,8 +1,11 @@
+import { Link } from "react-router-dom";
 
 const ErrorPages = () => {
     return (
-        <div>
-            ErrorPages
+        <div className="flex flex-col justify-center items-center h-screen gap-5">
+            <h1 className="text-5xl sm:text-7xl lg:text-9xl font-bold">404</h1>
+            <h1 className="text-xl sm:text-2xl lg:text-3xl font-semibold">Page not found</h1>
+            <Link to={'/'} className="btn btn-outline btn-neutral">Go Home</Link>
         </div>
     );
 };
